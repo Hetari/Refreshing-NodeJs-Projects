2c74f6e1e8e0f70d77714fd1b2eb5eacebcc5ef1
Ebraheem Alhetari <hetari4py@gmail.com>
2024-04-18 23:45:15 +0300

    finish the setup

--- controllers/task.js
+++ controllers/task.js
@@ -1,1 +1,21 @@
+const getAllTasks = (req, res) => {
+  res.send("All items");
+};
 
+const createTask = (req, res) => {
+  res.send("create");
+};
+
+const updateTask = (req, res) => {
+  res.send("update");
+};
+
+const getTask = (req, res) => {
+  res.send("get");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete");
+};
+
+export { getAllTasks, createTask, updateTask, getTask, deleteTask };
--- routes/task.js
+++ routes/task.js
@@ -1,8 +1,18 @@
 import express from "express";
+import {
+  getAllTasks,
+  createTask,
+  updateTask,
+  getTask,
+  deleteTask,
+} from "../controllers/task.js";
+
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-  res.send("Task manager app");
-});
+router.route("/").get(getAllTasks);
+router.route("/").post(createTask);
+router.route("/:id").get(getTask);
+router.route("/:id").patch(updateTask);
+router.route("/:id").delete(deleteTask);
 
 export { router };
