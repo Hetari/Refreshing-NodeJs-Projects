12161142fa7c55945c7e162279e05a7f7febaa08
Ebraheem Alhetari <hetari4py@gmail.com>
2024-05-02 21:52:05 +0300

    refactor: remove the static route

--- 2 store api/controllers/products.js
+++ 2 store api/controllers/products.js
@@ -3,7 +3,7 @@ import { isNumber } from '../functions/index.js';
 
 const pool2 = await connectToDatabase();
 
-const getAllProductsStatic = async (req, res) => {
+const getAllProducts = async (req, res) => {
   let { page, limit, fields, orderBy } = req.query;
 
   fields = fields ? fields.split(',') : ['*'];
@@ -56,23 +56,4 @@ const getAllProductsStatic = async (req, res) => {
   return res.status(200).json({ length: products.length, products });
 };
 
-const getAllProducts = async (req, res) => {
-  const params = req.body;
-  if (
-    'id' in params ||
-    'company' in params ||
-    'featured' in params ||
-    'name' in params
-  ) {
-    const product = await getProduct(pool2, params);
-    return res
-      .status(200)
-      .json({ len: product[0].length, product: product[0] });
-  } else {
-    throw new Error(
-      'Invalid request, please provide id, company name, featured state, or product name'
-    );
-  }
-};
-
-export { getAllProductsStatic, getAllProducts };
+export { getAllProducts };
--- 2 store api/routes/products.js
+++ 2 store api/routes/products.js
@@ -1,13 +1,8 @@
 import { Router } from 'express';
-import {
-  getAllProductsStatic,
-  getAllProducts,
-} from '../controllers/products.js';
+import { getAllProducts } from '../controllers/products.js';
 
 const productsRouter = Router();
 
 productsRouter.route('/').get(getAllProducts);
-
-productsRouter.route('/static').get(getAllProductsStatic);
 
 export { productsRouter };
