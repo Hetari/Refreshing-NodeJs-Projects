18640b41bdc433f796afc28c5dec4735d4f295c6
Hetari <hetari4py@gmail.com>
2024-06-09 16:52:12 +0300

    lean testing

--- 5 test/main.js
+++ 5 test/main.js
@@ -1,17 +1,17 @@
-const sum = (a, b) => {
-  if (typeof a == 'string' || typeof b == 'string') {
-    throw new Error("Args should not be 'string'");
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.post('/users', async (req, res) => {
+  const { password, username } = req.body;
+
+  if (!password || !username) {
+    res.sendStatus(400);
+    return;
   }
-  return a + b;
-};
 
-const fetchData = (callback) => {
-  setTimeout(() => {
-    callback('Done! here i your data');
-  }, 2000);
-};
+  res.send({ userId: 0 });
+});
 
-module.exports = {
-  sum,
-  fetchData,
-};
+export default app;
---
+++ 5 test/server.js
@@ -0,0 +1,3 @@
+import app from './main.js';
+
+app.listen(3000, () => console.log('server started'));
